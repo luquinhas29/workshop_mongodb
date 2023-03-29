4aa00b46628fdf0cc8807a8207eb6dde33157e07
Lucas williams <lucaswilliamscesar@gmail.com>
2023-03-29 09:05:37 -0300

    created DTO

--- src/main/java/com/lucaswilliams/workshopmongodb/resource/UserResource.java
+++ src/main/java/com/lucaswilliams/workshopmongodb/resource/UserResource.java
@@ -1,8 +1,8 @@
 package com.lucaswilliams.workshopmongodb.resource;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucaswilliams.workshopmongodb.domain.User;
+import com.lucaswilliams.workshopmongodb.dto.UserDTO;
 import com.lucaswilliams.workshopmongodb.service.UserService;
 
 @RestController
@@ -21,9 +22,10 @@ public class UserResource {
 	UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> dto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(dto);
 	}
 
 }
