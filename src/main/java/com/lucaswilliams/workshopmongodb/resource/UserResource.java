e266be7e4d008074c99626a4a8df4b52786991ab
Lucas williams <lucaswilliamscesar@gmail.com>
2023-04-01 08:46:05 -0300

    end point para retorna usuario

--- src/main/java/com/lucaswilliams/workshopmongodb/resource/UserResource.java
+++ src/main/java/com/lucaswilliams/workshopmongodb/resource/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.lucaswilliams.workshopmongodb.domain.Post;
 import com.lucaswilliams.workshopmongodb.domain.User;
 import com.lucaswilliams.workshopmongodb.dto.UserDTO;
 import com.lucaswilliams.workshopmongodb.service.UserService;
@@ -60,6 +61,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 
 }
